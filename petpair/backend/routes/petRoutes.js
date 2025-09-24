b8028a15caea2a1da5401584ab8acaa13df18365
Keerthana Panchumarthi <168259439+HelloKeerthana@users.noreply.github.com>
2025-09-24 10:06:36 +0530

    Delete petpair directory

--- petpair/backend/routes/petRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { suggestPet } = require('../controllers/petController');
-
-router.post('/suggest-pet', suggestPet);
-
-module.exports = router;
