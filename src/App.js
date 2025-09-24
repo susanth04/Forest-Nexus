0e930fcb6511dd03f7cd58d6b9d28cef5d367940
Keerthana Panchumarthi <168259439+HelloKeerthana@users.noreply.github.com>
2025-09-24 08:56:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import DSS from "./DSS.jsx";
-import OCR from "./OCR.jsx";
-import { OcrProvider } from "./context/OcrContext.js";
+import OCR_DSS from "./OCR_DSS.jsx"; 
 
 function App() {
   return (
-    <OcrProvider>
-      <Router>
-        <div className="App">
-          {/* Navbar */}
-          <nav className="navbar">
-            <h2 className="logo">FRA Dashboard</h2>
-            <ul className="nav-links">
-              <li>
-                <Link to="/">DSS</Link>
-              </li>
-              <li>
-                <Link to="/ocr">OCR</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* Page Content */}
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<DSS />} />
-              <Route path="/ocr" element={<OCR />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </OcrProvider>
+    <div className="App">
+      <OCR_DSS />
+    </div>
   );
 }
 
